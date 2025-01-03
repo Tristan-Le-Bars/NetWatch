4edf4238b98c12590b2edccbc9221fb9185641e8
Tristan-Le-Bars <tristan.lebars@protonmail.com>
2025-01-03 23:49:00 +0100

    have to push

---
+++ front_office/connection_socket.cpp
@@ -0,0 +1,5 @@
+#include "connection_socket.h"
+
+connection_socket::connection_socket() {
+
+}
---
+++ front_office/connection_socket.h
@@ -0,0 +1,13 @@
+#ifndef CONNECTION_SOCKET_H
+#define CONNECTION_SOCKET_H
+
+class connection_socket
+{
+public:
+    connection_socket();
+private:
+    int socket;
+
+};
+
+#endif // CONNECTION_SOCKET_H
