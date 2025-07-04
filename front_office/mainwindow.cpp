b38e41e65af6962a5f28528e04fcc0d19bbd5b93
Tristan-Le-Bars <tristan.lebars@protonmail.com>
2025-07-05 00:07:41 +0200

    [FIX] - give a proper name to the main window

--- front_office/mainwindow.cpp
+++ front_office/mainwindow.cpp
@@ -15,6 +15,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle("NetWatch");
 
     ui->scrollArea->setWidget(ui->scrollAreaWidgetContents);
     ui->scrollArea->setWidgetResizable(true);
