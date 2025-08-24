8a2c449159667c1d5f6b418eaa13bb7fafd1ba24
Tristan Le Bars <47105496+Tristan-Le-Bars@users.noreply.github.com>
2025-08-24 21:58:23 +0200

    Remove useless comments

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -23,18 +23,18 @@ Server::~Server(){}
 
 int Server::connexionSocket() {
     FileTools file_manager;
-    int server_fd;        // File descriptor pour le socket du serveur
-    int connexion_socket; // socket qui representera une connection entrante
+    int server_fd;
+    int connexion_socket;
     int valread;
-    struct sockaddr_in address;  // Structure pour stocker l'adresse du serveur // sockaddr_in represent l'adresse d'un socket
-    int opt = 1;          // Option utilisée pour configurer les options de socket
-    int addrlen = sizeof(address);  // Taille de la structure d'adresse
-    char server_ip[INET_ADDRSTRLEN]; // chaine de character contenant l'adresse ipv4 du server
-    char client_ip[INET_ADDRSTRLEN]; // chaine de character contenant l'adresse ipv4 du client
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char server_ip[INET_ADDRSTRLEN];
+    char client_ip[INET_ADDRSTRLEN];
     char buffer[1024] = {0};
 
 
-    // 1. Création du file descriptor pour le socket
+    // Création du file descriptor pour le socket
     // le mot clé socket créé un point de terminaison pour la communication réseau. AF_INET = utilisation du protocol ipv4. SOCK_STRAM = socket de type stream. 0 = choix automatique du protocol.
     // server_fd devient concretement un socket à ce moment là.
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -51,12 +51,11 @@ int Server::connexionSocket() {
     // server_fd = file descriptor sur lequel les options doivent être configurer.
     // SOL_SOCKET = niveau ou l'option est défini. ici au niveau du socket.
     // SO_REUSEADDR | SO_REUSEPORT = permert de réutiliser une adresse et un port.
-    // &opt = valeur à définir pour l'option ???
     // sizeof(opt) = taille en octet de la valeur de l'option
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");     // Affiche un message d'erreur en cas d'échec de la configuration des options
-        close(server_fd);         // Ferme le socket
-        exit(EXIT_FAILURE);       // Quitte le programme avec un code d'échec
+        perror("setsockopt");
+        close(server_fd);
+        exit(EXIT_FAILURE);
     }
     else {
         std::cout << "socket option setting successfull" << std::endl;
@@ -73,9 +72,9 @@ int Server::connexionSocket() {
     // (struct sockaddr*)&address = pointeur vers la structure contenant l'adresse et le port à associer.
     // sizeof(address) = taille en octet de la structure contenant l'adresse et le port.
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        perror("bind failed");    // Affiche un message d'erreur en cas d'échec du bind
-        close(server_fd);         // Ferme le socket
-        exit(EXIT_FAILURE);       // Quitte le programme avec un code d'échec
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
     }
     else{
         std::cout << "socket/addresse binding successfull" << std::endl;
@@ -90,19 +89,19 @@ int Server::connexionSocket() {
         perror("getsockname failed");
     }
 
-    // 4. Mise en écoute du socket
+    // Mise en écoute du socket
     // listen prépare le socket à accepter les connexions entrantes
     // server_fd = file descriptor du socket à mettre en écoute
     // 3 = taille maximal de la queu de connection
     if (listen(server_fd, 3) < 0) {
-        perror("listen");         // Affiche un message d'erreur en cas d'échec de la mise en écoute
-        close(server_fd);         // Ferme le socket
-        exit(EXIT_FAILURE);       // Quitte le programme avec un code d'échec
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
     }
 
-    std::cout << "Server is listening on port " << client_port << std::endl;  // Message indiquant que le serveur est en écoute
+    std::cout << "Server is listening on port " << client_port << std::endl;
 
-    // 5. Acceptation des connexions entrantes
+    // Acceptation des connexions entrantes
     // accept est utiliser pour autorisé la connection entrante d'un client.
     // connexion_socket = nouveau socket spécifique qui represente la connexion entrante.
     // server_fd = socket en écoute
@@ -117,15 +116,13 @@ int Server::connexionSocket() {
             perror("accept");
             continue;
         }
-        // Créez un thread pour chaque connexion client
         std::thread client_thread(&Server::clientHandler, this, connexion_socket, std::ref(client_address));
-        client_thread.detach();  // Détachez le thread pour qu'il puisse fonctionner indépendamment
+        client_thread.detach();
     }
-    // Fermeture du socket de la connexion client
     close(connexion_socket);
 
-    close(server_fd);  // Ferme le socket du serveur
-    return 0;          // Terminaison normale du programme
+    close(server_fd);
+    return 0;
 }
 
 int Server::monitoringSocket(){
@@ -165,13 +162,10 @@ int Server::monitoringSocket(){
     int new_socket;
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
         perror("Accept");
-        // continue;
     }
-    // Get the received data and send them to the front end
-    // std::lock_guard<std::mutex> lock(client_mutex);
+
     admin_sockets.push_back(new_socket);
     std::cout << "Front-end client connected." << std::endl;
-    // }
 }
 
 int Server::clientHandler(int client_socket, struct sockaddr_in client_address){
@@ -186,22 +180,16 @@ int Server::clientHandler(int client_socket, struct sockaddr_in client_address){
     while ((bytesRead = read(client_socket, buffer, sizeof(buffer))) > 0) {
         std::string command(buffer, bytesRead);
         if (command == "send_resources") {
-            // Read the incoming machine resources data
-            bytesRead = read(client_socket, buffer, 1024);  // Read the data into the buffer
+            bytesRead = read(client_socket, buffer, 1024);
             std::string resourcesData(buffer, bytesRead);
 
-            // Display the received data or process it as needed
             std::cout << "Received machine resources data from client:" << std::endl;
             std::cout << resourcesData << std::endl;
             
-            // Optionally, send a response back to the client
             std::string response = "Machine resources data received";
             send(client_socket, response.c_str(), response.size(), 0);
 
-            // ICI format le json pour tout envoyer d'un coup ?
 
-            // Send data to the admin front-end
-            // std::lock_guard<std::mutex> lock(client_mutex);  // Protège l'accès à frontend_clients
             std::cout << 'list of frontend sockets:' << std::endl;
             for (int frontend_socket : admin_sockets){
                 std::cout << "  - "<< frontend_socket << std::endl;
